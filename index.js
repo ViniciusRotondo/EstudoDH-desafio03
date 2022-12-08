4dda6a3ad46e8034e672dcf37745809b0f89b6c4
ViniciusRotondo <vinirotondo.trade@gmail.com>
2022-12-08 20:41:11 -0300

    meu segundo commit

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
+/* Precisamos deixar essa função mais flexível, ela deve aceitar qualquer quantidade de parâmetros e retornar a soma de todos os parâmetros.
+const somaNumeros = (numero1, numero2) => { 
+    return numero1 + numero2
+}
+ */
+
 function somar (...numeros){
     return numeros.reduce((item, elemento) => item += elemento)
 }
